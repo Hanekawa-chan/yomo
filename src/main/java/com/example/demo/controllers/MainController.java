3b7830b699988a88ce13247260bcd8b3ec9c409c
Tsubasa <52043350+Hanekawa-chan@users.noreply.github.com>
2021-01-18 20:20:27 +0300

    пофиксил переадресацию

--- src/main/java/com/example/demo/controllers/MainController.java
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -8,9 +8,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class MainController {
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
+    @GetMapping("/main")
+    public String main(){
+        return "main";
     }
+
+    @GetMapping("/team")
+    public String team(){
+        return "team";
+    }
+
 }
